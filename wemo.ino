5f0a53391b5f9e602f99bb3e6556c28fd978e677
Rodney Peck <ropeck@google.com>
2017-02-10 14:22:34 -0800

    working

--- wemo.ino
+++ wemo.ino
@@ -1,56 +1,33 @@
-// This #include statement was automatically added by the Particle IDE.
-#include <MyHttpClient.h>
 
-#include "application.h"
-
-/**
-* Declaring the variables.
-*/
-unsigned int nextTime = 0;    // Next time to contact the server
-MyHttpClient http;
-
-// Headers currently need to be set at init, useful for API keys etc.
-http_header_t headers[] = {
-    //  { "Content-Type", "application/json" },
-    //  { "Accept" , "application/json" },
-    { "Accept" , "*/*"},
-    { "Content-Type", "text/xml; charset=utf-8"},
-    {"SOAPACTION", "urn:Belkin:service:basicevent:1#SetBinaryState"},
-//    {"Connection", "keep-alive"},
-    { NULL, NULL } // NOTE: Always terminate headers will NULL
-};
-
-http_request_t request;
-http_response_t response;
+TCPClient client;
+char wemoIP[ ] = "10.0.1.10";
+int wemoPort = 49153;
 
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-    if (nextTime > millis()) {
-        return;
+  Serial.println("switchON");
+  String data1;
+  data1+="<?xml version=\"1.0\" encoding=\"utf-8\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:SetBinaryState xmlns:u=\"urn:Belkin:service:basicevent:1\"><BinaryState>1</BinaryState></u:SetBinaryState></s:Body></s:Envelope>"; // Use HTML encoding for comma's
+  TCPClient client;
+  if (client.connect(wemoIP,wemoPort)) {
+        client.println("POST /upnp/control/basicevent1 HTTP/1.1");
+        client.println("Content-Type: text/xml; charset=utf-8");
+        client.println("SOAPACTION: \"urn:Belkin:service:basicevent:1#SetBinaryState\"");
+        client.println("Connection: keep-alive");
+        client.print("Content-Length: ");
+        client.println(data1.length());
+        client.println();
+        client.print(data1);
+        client.println();
     }
 
-    Serial.println();
-    Serial.println("Application>\tStart of Loop.");
-    // Request path and body can be set at runtime or at setup.
-    request.hostname = "10.0.1.10";
-    request.port = 49153;
-    request.path = "/upnp/control/basicevent1";
-    request.body = "<?xml version=\"1.0\" encoding=\"utf-8\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:SetBinaryState xmlns:u=\"urn:Belkin:service:basicevent:1\"><BinaryState>0</BinaryState></u:SetBinaryState></s:Body></s:Envelope>"; 
-
-
-    http.post(request, response, headers);
-    Serial.print("body:");
-    Serial.print(request.body);
-    Serial.print("Application>\tResponse status: ");
-    Serial.println(response.status);
-
-    Serial.print("Application>\tHTTP Response Body: ");
-    Serial.println(response.body);
-
-    nextTime = millis() + 10000;
+  if (client.connected()) {
+     client.stop();
+  }
+  delay(1000);
 }
 
 
