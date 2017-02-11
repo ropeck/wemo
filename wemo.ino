67a9278cebecd781ac7bafa005d8dcaa695fdadd
Rodney Peck <ropeck@google.com>
2017-02-10 16:36:24 -0800

    working blinker

--- wemo.ino
+++ wemo.ino
@@ -1,4 +1,7 @@
 
+#define ON "1"
+#define OFF "0"
+
 TCPClient client;
 char wemoIP[ ] = "10.0.1.10";
 int wemoPort = 49153;
@@ -7,11 +10,14 @@ void setup() {
     Serial.begin(9600);
 }
 
-void loop() {
-  Serial.println("switchON");
-  String data1;
-  data1+="<?xml version=\"1.0\" encoding=\"utf-8\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:SetBinaryState xmlns:u=\"urn:Belkin:service:basicevent:1\"><BinaryState>1</BinaryState></u:SetBinaryState></s:Body></s:Envelope>"; // Use HTML encoding for comma's
+void switchSet(String state) {
   TCPClient client;
+  String data1;
+  data1 += "<?xml version=\"1.0\" encoding=\"utf-8\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:SetBinaryState xmlns:u=\"urn:Belkin:service:basicevent:1\"><BinaryState>";
+  data1 += state;
+  data1 += "%d</BinaryState></u:SetBinaryState></s:Body></s:Envelope>";
+
+  Serial.println("switchState");
   if (client.connect(wemoIP,wemoPort)) {
         client.println("POST /upnp/control/basicevent1 HTTP/1.1");
         client.println("Content-Type: text/xml; charset=utf-8");
@@ -27,7 +33,20 @@ void loop() {
   if (client.connected()) {
      client.stop();
   }
-  delay(1000);
 }
 
+void switchOn() {
+  switchSet(ON);
+}
+
+void switchOff() {
+  switchSet(OFF);
+}
+
+void loop() {
+  switchOn();
+  delay(1000);
+  switchOff();
+  delay(1000);
+}
 
